978038733fd755acb6dda96f774b9e97978ea39c
Habib Akcakale <habibakcakale@rubygamestudio.com>
2020-09-11 15:00:54 +0300

    Angular PWA update logic added to project.

--- FoodApp/src/app/app.component.ts
+++ FoodApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {ApplicationRef, Component} from '@angular/core';
+import {SwUpdate} from "@angular/service-worker";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FoodApp';
+
+  constructor(appRef: ApplicationRef, update: SwUpdate) {
+    let interval = setInterval(() => {
+      if (update.isEnabled) {
+        clearInterval(interval);
+        return this.updateApp(update);
+      }
+    }, 1000);
+  }
+
+  updateApp(update: SwUpdate) {
+    update.available.subscribe(e => {
+      update.activateUpdate().then(e => location.reload())
+    });
+    return update.checkForUpdate();
+  }
 }
