9808bfd7b3469d10f382c5f9f5baee9b8a7903c7
Habib Akcakale <habibakcakale@rubygamestudio.com>
2021-03-28 21:34:53 +0300

    App Component Update check.

--- src/meal/ClientApp/src/app/app.component.ts
+++ src/meal/ClientApp/src/app/app.component.ts
@@ -2,26 +2,28 @@ import {ApplicationRef, Component} from '@angular/core';
 import {SwUpdate} from "@angular/service-worker";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ClientApp';
+    title = 'ClientApp';
 
-  constructor(appRef: ApplicationRef, update: SwUpdate) {
-    let interval = setInterval(() => {
-      if (update.isEnabled) {
-        clearInterval(interval);
-        return this.updateApp(update);
-      }
-    }, 1000);
-  }
+    constructor(appRef: ApplicationRef, update: SwUpdate) {
+        let interval = setInterval(() => {
+            console.log(`update enabled ${update.isEnabled}`);
+            if (update.isEnabled) {
+                clearInterval(interval);
+                return this.updateApp(update);
+            }
+        }, 1000);
+    }
 
-  updateApp(update: SwUpdate) {
-    update.available.subscribe(e => {
-      update.activateUpdate().then(e => location.reload())
-    });
-    return update.checkForUpdate();
-  }
+    updateApp(update: SwUpdate) {
+        update.available.subscribe(e => {
+            console.log(`update available`, e);
+            update.activateUpdate().then(e => location.reload())
+        });
+        return update.checkForUpdate();
+    }
 }
