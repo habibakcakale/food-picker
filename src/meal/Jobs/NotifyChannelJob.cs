2042fe01a393c7e7cf677565b3617950c083c806
Habib Akcakale <habibakcakale@rubygamestudio.com>
2021-04-05 22:57:07 +0300

    Auto Select Meal for lazy people.

--- src/meal/Jobs/NotifyChannelJob.cs
+++ src/meal/Jobs/NotifyChannelJob.cs
@@ -11,6 +11,7 @@ namespace Meal.Jobs {
     using Models;
     using Quartz;
     using SlackAPI;
+    using User = Models.User;
 
     public class NotifyChannelJob : IJob {
         private readonly FoodDbContext dbContext;
@@ -31,20 +32,53 @@ namespace Meal.Jobs {
                 join order in dbContext.Orders.Where(item => item.Date == DateTime.Today) on user.Id equals order.UserId into orders
                 from todayOrder in orders.DefaultIfEmpty()
                 where !string.IsNullOrWhiteSpace(user.SlackId) && todayOrder == null
-                select user.SlackId;
+                select new User {Id = user.Id, SlackId = user.SlackId};
             var users = await query.ToListAsync();
-            await NotifyChannel(users);
+            if (users.Any()) {
+                var topPicks = await GetTopPicks();
+                await SelectMealForUsers(users, topPicks);
+                await NotifyChannel(users, topPicks);
+            }
         }
 
-        private Task NotifyChannel(IReadOnlyCollection<string> users) {
-            var taskCompletionSource = new TaskCompletionSource();
+        private async Task SelectMealForUsers(IEnumerable<User> users, IReadOnlyCollection<OrderItem> topPicks) {
+            var orders = users.Select(user => new Order {
+                UserId = user.Id,
+                Date = DateTime.UtcNow.Date,
+                OrderItems = topPicks.Select(item => new OrderItem {
+                    Name = item.Name,
+                    MealType = item.MealType
+                }).ToList()
+            });
+            await dbContext.Orders.AddRangeAsync(orders);
+            await dbContext.SaveChangesAsync();
+        }
+
+        private async Task<List<OrderItem>> GetTopPicks() {
+            var topPicks = await dbContext.Orders
+                .Where(item => item.Date == DateTime.Today)
+                .SelectMany(item => item.OrderItems)
+                .GroupBy(item => new {item.MealType, item.Name})
+                .Select(item => new {item.Key.MealType, item.Key.Name, Count = item.Count()})
+                .OrderBy(item => item.MealType).ThenByDescending(item => item.Count)
+                .ToListAsync();
+            return topPicks
+                .GroupBy(item => item.MealType)
+                .SelectMany(item => item.Take(item.Key == MealType.Salad ? 2 : 1))
+                .Select(item => new OrderItem {Name = item.Name, MealType = item.MealType})
+                .ToList();
+        }
+
+        private Task NotifyChannel(ICollection<User> users, IEnumerable<OrderItem> topPicks) {
             if (!users.Any()) {
-                return taskCompletionSource.Task;
+                return Task.CompletedTask;
             }
 
+            var taskCompletionSource = new TaskCompletionSource();
+
             var builder = new StringBuilder();
-            foreach (var slackId in users) {
-                builder.AppendFormat("<@{0}>{1}", slackId, users.Last() == slackId ? string.Empty : ", ");
+            foreach (var user in users) {
+                builder.AppendFormat("<@{0}>{1}", user.SlackId, users.Last() == user ? string.Empty : ", ");
             }
 
             slackClient.PostMessage(response => {
@@ -54,9 +88,17 @@ namespace Meal.Jobs {
                         logger.LogError("Error occured while sending slack message {SlackError}", response.error);
                         taskCompletionSource.TrySetException(new Exception(response.error));
                     }
-                }, slackOptions.Channel, string.Empty,
-                attachments: new[] {new Attachment {color = "#c2185b", pretext = "Still have time to pick.", text = builder.ToString()}},
-                botName: "Pick Food!");
+                },
+                slackOptions.Channel,
+                string.Empty,
+                attachments: new[] {
+                    new Attachment {
+                        color = "#c2185b",
+                        pretext = $"You still have time to pick. We already picked {string.Join(", ", topPicks.Select(item => item.Name))} for you. But you can change it. :tada:",
+                        text = builder.ToString()
+                    }
+                },
+                botName: "Ruby Chef!");
 
             return taskCompletionSource.Task;
         }
